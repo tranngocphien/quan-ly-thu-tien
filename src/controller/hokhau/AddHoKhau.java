6cae3e43df1f7073f1f5abad4c4f2691ddeff427
Phien <tranngocphien0309@gmail.com>
2020-12-05 17:48:41 +0700

    update addHoKhau.fxml

--- src/controller/hokhau/AddHoKhau.java
+++ src/controller/hokhau/AddHoKhau.java
@@ -1,23 +1,35 @@
 package controller.hokhau;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class AddHoKhau {
-    @FXML
-    private TextField tfMaChuHo;
-    @FXML
-    private TextField tfMaHoKhau;
-    @FXML
-    private TextField tfSoThanhVien;
-    @FXML
-    private TextField tfDiaChi;
-    @FXML
-    private TextField tfSoDienThoai;
+	   @FXML
+	    private TextField tfMaChuHo;
 
-    public void addHoKhau() {
-    	
-    }
-    
+	    @FXML
+	    private TextField tfMaHoKhau;
+
+	    @FXML
+	    private TextField tfTenChuHo;
+
+	    @FXML
+	    private TextField tfDiaChi;
+
+	    @FXML
+	    private TextField tfTuoi;
+
+	    @FXML
+	    private TextField tfCMND;
+
+	    @FXML
+	    private TextField tfSoDienThoai;
+
+	    @FXML
+	    public void addHoKhau(ActionEvent event) {
+	    
+	    }
+
     
 }
