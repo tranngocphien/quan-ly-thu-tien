1cb4589e10b3143122a5add9c0c12e03b34d775a
Huy Nguyen <trannguyenhan01092000@gmail.com>
2020-12-05 22:32:17 +0700

    4/12/2020

--- src/controller/HoKhauController.java
+++ src/controller/HoKhauController.java
@@ -3,11 +3,13 @@ package controller;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
@@ -20,8 +22,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -46,67 +50,69 @@ public class HoKhauController implements Initializable {
 	TableColumn<HoKhauModel, String> colDiaChi;
 	@FXML
 	TableView<HoKhauModel> tvHoKhau;
+	@FXML
+	TextField tfSearch;
 
 	@FXML
 	ComboBox<String> cbChooseSearch;
-	
+
 	ObservableList<HoKhauModel> listValueTableView;
 	private List<HoKhauModel> listHoKhau;
-	
+
 	// Hien thi thong tin ho khau
 	public void showHoKhau() throws ClassNotFoundException, SQLException {
 		listHoKhau = new HoKhauService().getListHoKhau();
 		listValueTableView = FXCollections.observableArrayList(listHoKhau);
 		List<NhanKhauModel> listNhanKhau = new NhanKhauService().getListNhanKhau();
 		List<ChuHoModel> listChuHo = new ChuHoService().getListChuHo();
-		
+
 		Map<Integer, Integer> mapMahoToId = new HashMap<>();
 		listChuHo.forEach(chuho -> {
 			mapMahoToId.put(chuho.getMaHo(), chuho.getIdChuHo());
 		});
-		
+
 		Map<Integer, String> mapIdToTen = new HashMap<>();
 		listNhanKhau.forEach(nhankhau -> {
 			mapIdToTen.put(nhankhau.getId(), nhankhau.getTen());
 		});
-		
+
 		// Thiet lap Table views
 		colMaHoKhau.setCellValueFactory(new PropertyValueFactory<HoKhauModel, String>("maHo"));
-		colMaChuHo.setCellValueFactory(
-				(CellDataFeatures<HoKhauModel, String> p) -> new ReadOnlyStringWrapper(mapIdToTen.get(mapMahoToId.get(p.getValue().getMaHo())).toString())
-		);
+		colMaChuHo.setCellValueFactory((CellDataFeatures<HoKhauModel, String> p) -> new ReadOnlyStringWrapper(
+				mapIdToTen.get(mapMahoToId.get(p.getValue().getMaHo())).toString()));
 		colSoThanhVien.setCellValueFactory(new PropertyValueFactory<HoKhauModel, String>("soThanhvien"));
 		colDiaChi.setCellValueFactory(new PropertyValueFactory<HoKhauModel, String>("diaChi"));
 		tvHoKhau.setItems(listValueTableView);
 
 		// Thiet lap Combo box
-		ObservableList<String> listComboBox = FXCollections.observableArrayList("Mã hộ", "Id chủ hộ", "Địa chỉ");
-		cbChooseSearch.setValue("Chọn");
+		ObservableList<String> listComboBox = FXCollections.observableArrayList("Mã hộ", "Tên chủ hộ", "Địa chỉ");
+		cbChooseSearch.setValue("Mã hộ");
 		cbChooseSearch.setItems(listComboBox);
 	}
 
 	public void addHoKhau() throws ClassNotFoundException, SQLException, IOException {
 		Parent home = FXMLLoader.load(getClass().getResource("/views/hokhau/AddHoKhau.fxml"));
-        Stage stage = new Stage();
-        stage.setScene(new Scene(home,800,600));
-        stage.setResizable(false);
-        stage.showAndWait();
-        showHoKhau();
+		Stage stage = new Stage();
+		stage.setScene(new Scene(home, 800, 600));
+		stage.setResizable(false);
+		stage.showAndWait();
+		showHoKhau();
 	}
 
 	public void delHoKhau() throws ClassNotFoundException, SQLException {
 		HoKhauModel hoKhauModel = tvHoKhau.getSelectionModel().getSelectedItem();
-		
-		if(hoKhauModel == null) {
+
+		if (hoKhauModel == null) {
 			Alert alert = new Alert(AlertType.WARNING, "Hãy chọn hộ khẩu bạn muốn xóa!", ButtonType.OK);
 			alert.setHeaderText(null);
 			alert.showAndWait();
 		} else {
-			Alert alert = new Alert(AlertType.WARNING, "Khi xóa hộ khẩu, tất cả thành viên trong hộ đều sẽ bị xóa!", ButtonType.YES, ButtonType.NO);
+			Alert alert = new Alert(AlertType.WARNING, "Khi xóa hộ khẩu, tất cả thành viên trong hộ đều sẽ bị xóa!",
+					ButtonType.YES, ButtonType.NO);
 			alert.setHeaderText("Bạn có chắc chắn muốn xóa hộ khẩu này");
 			Optional<ButtonType> result = alert.showAndWait();
-			
-			if(result.get() == ButtonType.NO) {
+
+			if (result.get() == ButtonType.NO) {
 				return;
 			} else {
 				/// tao map anh xa gia tri Id sang maHo
@@ -115,27 +121,146 @@ public class HoKhauController implements Initializable {
 				listQuanHe.forEach(quanhe -> {
 					mapIdToMaho.put(quanhe.getIdThanhVien(), quanhe.getMaHo());
 				});
-				
-				int idHoKhauDel = hoKhauModel.getMaHo();
+
 				// xoa toan bo nhan khau trong ho khau
+				int idHoKhauDel = hoKhauModel.getMaHo(); // lay ra ma ho de so sanh
 				List<NhanKhauModel> listNhanKhauModels = new NhanKhauService().getListNhanKhau();
-				listNhanKhauModels.stream().filter(nhankhau -> mapIdToMaho.get(nhankhau.getId()) == idHoKhauDel).forEach(nk -> {
-					try {
-						new NhanKhauService().del(nk.getId());
-					} catch (ClassNotFoundException | SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				});
-				
+				listNhanKhauModels.stream().filter(nhankhau -> mapIdToMaho.get(nhankhau.getId()) == idHoKhauDel)
+						.forEach(nk -> {
+							try {
+								new NhanKhauService().del(nk.getId());
+							} catch (ClassNotFoundException | SQLException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						});
+
 				// xoa ho khau
 				new HoKhauService().del(idHoKhauDel);
 			}
 		}
-		
+
 		showHoKhau();
 	}
-		
+
+	public void searchHoKhau() throws ClassNotFoundException, SQLException {
+		ObservableList<HoKhauModel> listValueTableView_tmp = null;
+		String keySearch = tfSearch.getText();
+
+		// lay lua chon tim kiem cua khach hang
+		SingleSelectionModel<String> typeSearch = cbChooseSearch.getSelectionModel();
+		String typeSearchString = typeSearch.getSelectedItem();
+
+		// tim kiem thong tin theo lua chon da lay ra
+		switch (typeSearchString) {
+		case "Tên chủ hộ": {
+			// neu khong nhap gi -> thong bao loi
+			if (keySearch.length() == 0) {
+				tvHoKhau.setItems(listValueTableView);
+				Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào thông tin cần tìm kiếm!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+				break;
+			}
+			
+			Map<Integer, Integer> mapMahoToId = new HashMap<>();
+			List<ChuHoModel> listChuHo = new ChuHoService().getListChuHo();
+			listChuHo.stream().forEach(chuho -> {
+				mapMahoToId.put(chuho.getMaHo(), chuho.getIdChuHo());
+			});
+			Map<Integer, String> mapIdToTen = new HashMap<>();
+			List<NhanKhauModel> listNhanKhau = new NhanKhauService().getListNhanKhau();
+			listNhanKhau.stream().forEach(nhankhau -> {
+				mapIdToTen.put(nhankhau.getId(), nhankhau.getTen());
+			});
+			
+			int index = 0;
+			List<HoKhauModel> listHoKhauModelsSearch = new ArrayList<>();
+			for (HoKhauModel hoKhauModel : listHoKhau) {
+				if (mapIdToTen.get(mapMahoToId.get(hoKhauModel.getMaHo())).contains(keySearch)) {
+					listHoKhauModelsSearch.add(hoKhauModel);
+					index++;
+				}
+			}
+			listValueTableView_tmp = FXCollections.observableArrayList(listHoKhauModelsSearch);
+			tvHoKhau.setItems(listValueTableView_tmp);
+
+			// neu khong tim thay thong tin can tim kiem -> thong bao toi nguoi dung khong
+			// tim thay
+			if (index == 0) {
+				tvHoKhau.setItems(listValueTableView); // hien thi toan bo thong tin
+				Alert alert = new Alert(AlertType.INFORMATION, "Không tìm thấy thông tin!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+			}
+			break;
+		}
+		case "Địa chỉ": {
+			// neu khong nhap gi -> thong bao loi
+			if (keySearch.length() == 0) {
+				tvHoKhau.setItems(listValueTableView);
+				Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào thông tin cần tìm kiếm!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+				break;
+			}
+
+			int index = 0;
+			List<HoKhauModel> listHoKhau_tmp = new ArrayList<>();
+			for (HoKhauModel hoKhauModel : listHoKhau) {
+				if (hoKhauModel.getDiaChi().contains(keySearch)) {
+					listHoKhau_tmp.add(hoKhauModel);
+					index++;
+				}
+			}
+			listValueTableView_tmp = FXCollections.observableArrayList(listHoKhau_tmp);
+			tvHoKhau.setItems(listValueTableView_tmp);
+
+			// neu khong tim thay thong tin tim kiem -> thong bao toi nguoi dung
+			if (index == 0) {
+				tvHoKhau.setItems(listValueTableView); // hien thi toan bo thong tin
+				Alert alert = new Alert(AlertType.INFORMATION, "Không tìm thấy thông tin!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+			}
+			break;
+		}
+		default: { // truong hop con lai : tim theo id
+			// neu khong nhap gi -> thong bao loi
+			if (keySearch.length() == 0) {
+				tvHoKhau.setItems(listValueTableView);
+				Alert alert = new Alert(AlertType.INFORMATION, "Bạn cần nhập vào thông tin tìm kiếm!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+				break;
+			}
+
+			// kiem tra thong tin tim kiem co hop le hay khong
+			Pattern pattern = Pattern.compile("\\d{1,}");
+			if (!pattern.matcher(keySearch).matches()) {
+				Alert alert = new Alert(AlertType.WARNING, "Bạn phải nhập vào 1 số!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+				return;
+			}
+
+			for (HoKhauModel hoKhauModel : listHoKhau) {
+				if (hoKhauModel.getMaHo() == Integer.parseInt(keySearch)) {
+					listValueTableView_tmp = FXCollections.observableArrayList(hoKhauModel);
+					tvHoKhau.setItems(listValueTableView_tmp);
+					return;
+				}
+			}
+
+			// khong tim thay thong tin -> thong bao toi nguoi dung
+			tvHoKhau.setItems(listValueTableView);
+			Alert alert = new Alert(AlertType.WARNING, "Không tìm thấy thông tin!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+		}
+		}
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		try {
@@ -144,7 +269,7 @@ public class HoKhauController implements Initializable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
--- src/controller/hokhau/AddHoKhau.java
+++ src/controller/hokhau/AddHoKhau.java
@@ -1,35 +1,154 @@
 package controller.hokhau;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+import models.ChuHoModel;
+import models.HoKhauModel;
+import models.NhanKhauModel;
+import models.QuanHeModel;
+import services.ChuHoService;
+import services.HoKhauService;
+import services.NhanKhauService;
+import services.QuanHeService;
 
 public class AddHoKhau {
-	   @FXML
-	    private TextField tfMaChuHo;
+	@FXML
+	private TextField tfMaHoKhau;
+	@FXML
+	private TextField tfDiaChi;
+	@FXML
+	private TextField tfMaChuHo;
+	@FXML
+	private TextField tfTenChuHo;
+	@FXML
+	private TextField tfTuoi;
+	@FXML
+	private TextField tfCMND;
+	@FXML
+	private TextField tfSoDienThoai;
 
-	    @FXML
-	    private TextField tfMaHoKhau;
+	@FXML
+	public void addHoKhau(ActionEvent event) throws ClassNotFoundException, SQLException {
+		// khai bao mot mau de so sanh
+		Pattern pattern;
 
-	    @FXML
-	    private TextField tfTenChuHo;
+		// kiem tra maHo nhap vao
+		// maHo la day so tu 1 toi 11 chu so
+		pattern = Pattern.compile("\\d{1,11}");
+		if (!pattern.matcher(tfMaHoKhau.getText()).matches()) {
+			Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào mã hộ khẩu hợp lệ!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return;
+		}
+		// kiem tra maHo co bi trung voi maHo nao da ton tai truoc do hay khong
+		List<HoKhauModel> listHoKhauModels = new HoKhauService().getListHoKhau();
+		listHoKhauModels.stream().forEach(hokhau -> {
+			if (hokhau.getMaHo() == Integer.parseInt(tfMaHoKhau.getText())) {
+				Alert alert = new Alert(AlertType.WARNING, "Mã hộ bị trùng với một hộ khác!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+				return;
+			}
+		});
 
-	    @FXML
-	    private TextField tfDiaChi;
+		// kiem tra dia chi nhap vao
+		// dia chi nhap vao la 1 chuoi t 1 toi 30 ki tu
+		if (tfDiaChi.getText().length() >= 50 || tfDiaChi.getText().length() <= 1) {
+			Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào 1 địa chỉ hợp lệ!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return;
+		}
 
-	    @FXML
-	    private TextField tfTuoi;
+		// kiem tra id nhan khau nhap vao
+		// id la day so tu 1 toi 11 chu so
+		pattern = Pattern.compile("\\d{1,11}");
+		if (!pattern.matcher(tfMaChuHo.getText()).matches()) {
+			Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào mã nhân khẩu hợp lệ!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return;
+		}
+		// kiem tra ID them moi co bi trung voi nhung ID da ton tai hay khong
+		List<NhanKhauModel> listNhanKhauModels = new NhanKhauService().getListNhanKhau();
+		listNhanKhauModels.stream().forEach(nhankhau -> {
+			if (nhankhau.getId() == Integer.parseInt(tfMaChuHo.getText())) {
+				Alert alert = new Alert(AlertType.WARNING, "ID bị trùng với một người khác!", ButtonType.OK);
+				alert.setHeaderText(null);
+				alert.showAndWait();
+				return;
+			}
+		});
 
-	    @FXML
-	    private TextField tfCMND;
+		// kiem tra ten nhap vao
+		// ten nhap vao la chuoi tu 1 toi 50 ki tu
+		if (tfTenChuHo.getText().length() >= 50 || tfTenChuHo.getText().length() <= 1) {
+			Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào 1 tên hợp lệ!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return;
+		}
 
-	    @FXML
-	    private TextField tfSoDienThoai;
+		// kiem tra tuoi nhap vao
+		// tuoi nhap vao nhieu nhat la 1 so co 3 chu so
+		pattern = Pattern.compile("\\d{1,3}");
+		if (!pattern.matcher(tfTuoi.getText()).matches()) {
+			Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào tuổi hợp lệ!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return;
+		}
 
-	    @FXML
-	    public void addHoKhau(ActionEvent event) {
-	    
-	    }
+		// kiem tra cmnd nhap vao
+		// cmnd nhap vao phai la mot day so tu 1 toi 20 so
+		pattern = Pattern.compile("\\d{1,20}");
+		if (!pattern.matcher(tfCMND.getText()).matches()) {
+			Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào CMND hợp lệ!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return;
+		}
 
-    
+		// kiem tra sdt nhap vao
+		// SDT nhap vao phai khong chua chu cai va nho hon 15 chu so
+		pattern = Pattern.compile("\\d{1,15}");
+		if (!pattern.matcher(tfSoDienThoai.getText()).matches()) {
+			Alert alert = new Alert(AlertType.WARNING, "Hãy nhập vào SĐT hợp lệ!", ButtonType.OK);
+			alert.setHeaderText(null);
+			alert.showAndWait();
+			return;
+		}
+		
+		// ghi nhan cac gia tri khi tat ca deu hop le
+		int maHo = Integer.parseInt(tfMaHoKhau.getText());
+		String diaChi = tfDiaChi.getText();
+		int maChuHo = Integer.parseInt(tfMaChuHo.getText());
+		String tenChuHo = tfTenChuHo.getText();
+		int tuoiChuHo = Integer.parseInt(tfTuoi.getText());
+		String cmndChuHo = tfCMND.getText();
+		String sdtChuHo = tfSoDienThoai.getText();
+		
+		HoKhauModel hoKhauModel = new HoKhauModel(maHo, 0, diaChi);
+		NhanKhauModel nhanKhauModel = new NhanKhauModel(maChuHo, cmndChuHo, tenChuHo, tuoiChuHo, sdtChuHo);
+		
+		new HoKhauService().add(hoKhauModel);
+		new NhanKhauService().add(nhanKhauModel);
+		new QuanHeService().add(new QuanHeModel(maHo,maChuHo,"Là chủ hộ"));
+		new ChuHoService().add(new ChuHoModel(maHo, maChuHo));
+		
+		Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+	}
+
 }
