04a8c9468424ef9218a7cad2be8db9ed07440b74
Phien <tranngocphien0309@gmail.com>
2020-12-20 00:28:42 +0700

    update main and statistic

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,40 @@
 package controller;
 
-public class MainController {
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import models.HoKhauModel;
+import models.KhoanThuModel;
+import services.HoKhauService;
+import services.KhoanThuService;
+
+public class MainController implements Initializable{
+	@FXML
+	private Label lbSoHoKhau;
+
+	@FXML
+	private Label lbSoKhoanThu;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		try {
+			List<HoKhauModel> listHoKhau = new HoKhauService().getListHoKhau();
+			long soHoKhau = listHoKhau.stream().count();
+			lbSoHoKhau.setText(Long.toString(soHoKhau));
+			
+			List<KhoanThuModel> listKhoanThu = new KhoanThuService().getListKhoanThu();
+			long soKhoanThu = listKhoanThu.stream().count();
+			lbSoKhoanThu.setText(Long.toString(soKhoanThu));
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
